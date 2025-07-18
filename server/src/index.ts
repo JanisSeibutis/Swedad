d321193547ef908e0f9746c5d56b41f594154cbf
JanisSeibutis <janis.seibutis@gmail.com>
2025-07-18 10:32:07 +0200

    chore: update prisma tables

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,16 +12,66 @@ app.get("/", (_, res: Response) => {
   res.send("Hello World")
 })
 
-const testUser = async () => {
-  const users = await prisma.user.findMany()
+// const testUser = async () => {
+//   const users = await prisma.user.findMany()
 
-  await prisma.user.create({
-    data: {
-      name: "janis",
-      email: "janka_laika@testuser.com",
-    },
-  })
-}
+//   await prisma.user.create({
+//     data: {
+//       name: "janis",
+//       lastname: "laika",
+//       username: "jankalaika",
+//       email: "janka_laika@testuser.com",
+//       phone: "0761482888",
+//     },
+//   })
+// }
+
+// const testRegion = async () => {
+//   await prisma.region.create({
+//     data: {
+//       name: "Gävleborg",
+//     },
+//   })
+// }
+
+// const testCategory = async () => {
+//   await prisma.category.create({
+//     data: {
+//       name: "Strumpor",
+//     },
+//   })
+// }
+
+// const testAd = async (userId, categoryId, regionId) => {
+//   await prisma.ad.findMany()
+
+//   await prisma.ad.create({
+//     data: {
+//       userId,
+//       categoryId,
+//       regionId,
+//       title: "Great product",
+//       description: "Description here",
+//       price: 100,
+//       location: "Stockholm",
+//       images: {
+//         create: [{ url: "image1.jpg" }, { url: "image2.jpg" }],
+//       },
+//     },
+//   })
+// }
+
+// const mainTest = async () => {
+//   await testCategory()
+//   await testRegion()
+//   const user = await prisma.user.findFirst()
+//   const category = await prisma.category.findFirst()
+//   const region = await prisma.region.findFirst()
+
+//   await testAd(user?.id, category?.id, region?.id)
+// }
+
+// mainTest().catch(console.error)
 
 app.listen(PORT, () =>
   console.log(`Server is running at http://localhost: ${PORT}`)
