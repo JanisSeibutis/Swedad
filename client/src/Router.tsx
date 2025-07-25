cd04e016f8a9f9752218a1368cb463952b16827f
JanisSeibutis <janis.seibutis@gmail.com>
2025-07-25 22:58:20 +0200

    feat (client): create routes

---
+++ client/src/Router.tsx
@@ -0,0 +1,51 @@
+import { createBrowserRouter } from "react-router"
+import { Layout } from "./components/Layout"
+import { Home } from "./pages/Home"
+import { Login } from "./pages/Login"
+import { Register } from "./pages/Register"
+import { Ads } from "./pages/Ads"
+import { User } from "./pages/User"
+import { Ad } from "./components/Ad"
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/auth",
+        children: [
+          {
+            path: "login",
+            element: <Login />,
+          },
+          {
+            path: "register",
+            element: <Register />,
+          },
+        ],
+      },
+      {
+        path: "ads",
+        children: [
+          {
+            path: "",
+            element: <Ads />,
+          },
+          {
+            path: ":id",
+            element: <Ad />,
+          },
+        ],
+      },
+      {
+        path: "/user",
+        element: <User />,
+      },
+    ],
+  },
+])
