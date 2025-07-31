430d12b6e4456c0eed4aeb0b94eafac9e121d33a
JanisSeibutis <janis.seibutis@gmail.com>
2025-07-31 13:33:16 +0200

    chore (server): remove prisma migration files

--- server/prisma/migrations/20250716091504_prevent_duplicate_ads/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[title,userId]` on the table `Ad` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Ad_title_userId_key" ON "Ad"("title", "userId");
