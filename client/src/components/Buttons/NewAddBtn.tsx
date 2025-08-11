f9a3564d942c0941d0bb9b193cfc8a5ab26fef98
JanisSeibutis <janis.seibutis@gmail.com>
2025-08-11 13:03:22 +0200

    feat (client): create nav btn for new ads

---
+++ client/src/components/Buttons/NewAddBtn.tsx
@@ -0,0 +1,11 @@
+import "./NewAddBtn.css"
+
+export const NewAddBtn = () => {
+  return (
+    <>
+      <button className="custom-button">
+        <span className="icon">+</span> Lägg in annons
+      </button>
+    </>
+  )
+}
