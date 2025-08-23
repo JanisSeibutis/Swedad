c67b80c40ef0ea120612e5f74b176b63196783f7
JanisSeibutis <janis.seibutis@gmail.com>
2025-08-23 18:23:59 +0200

    feat (client): add submit btn to form and handle submit

--- client/src/components/Forms/SearchAdsForm.tsx
+++ client/src/components/Forms/SearchAdsForm.tsx
@@ -1,16 +1,36 @@
+import { useState, type FormEvent } from "react"
 import { SearchIcon } from "../Icons/SearchIcon"
 import { RegionSelectContainer } from "../RegionSelectContainer"
 import "./SearchAdsForm.css"
+import { Checkmark } from "../Icons/Checkmark"
+import { FindAds } from "../Buttons/FindAds"
+import { useNavigate } from "react-router"
+import type { IRegion } from "../../models/IReagion"
 
 export const SearchAdsForm = () => {
+  const [isActive, setIsActive] = useState(false)
+  const [search, setSearch] = useState("")
+  const [region, setRegion] = useState<IRegion>({
+    id: "",
+    name: "",
+  })
+  const navigate = useNavigate()
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    navigate(`/ads?region=${region.id}`)
+  }
+
   return (
     <div className="hero-wrap">
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-wrap">
           <span className="search">Sök</span>
           <div className="searchinput-wrap">
             <SearchIcon />
             <input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
               type="text"
               className="search-input"
               placeholder="Vad vill du söka efter?"
@@ -18,8 +38,27 @@ export const SearchAdsForm = () => {
           </div>
           <span className="search">Välj plats</span>
           <div className="regioninput-wrap">
-            <RegionSelectContainer />
+            <RegionSelectContainer setRegion={setRegion} />
           </div>
+          <span className="search">Eller hitta saker som kan skickas</span>
+          <div className="shipping">
+            <label className="shipping-wrap">
+              <span
+                className={
+                  isActive ? "custom-checkbox checked" : "custom-checkbox"
+                }
+              >
+                {isActive && <Checkmark />}
+              </span>
+              <input
+                type="checkbox"
+                checked={isActive}
+                onChange={() => setIsActive(!isActive)}
+              />
+              Visa bara annonser med frakt
+            </label>
+          </div>
+          <FindAds />
         </div>
       </form>
     </div>
---
+++ client/src/components/Icons/Checkmark.tsx
@@ -0,0 +1,19 @@
+export const Checkmark = () => {
+  return (
+    <svg
+      viewBox="0 0 32 32"
+      fill="none"
+      width="16"
+      height="16"
+      color="white"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12.23 25.431l-7.678-7.679a1.881 1.881 0 112.661-2.661l5.018 5.017L24.787 7.552a1.88 1.88 0 012.661 0 1.88 1.88 0 010 2.662L12.231 25.43z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
