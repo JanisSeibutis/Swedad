5c0e613e72be638c74f05c1ed77ab8af5c4cbe63
JanisSeibutis <janis.seibutis@gmail.com>
2025-08-23 18:20:51 +0200

    refactor (client): clean up imports & make components of svgs

--- client/src/components/NavCategoryList.tsx
+++ client/src/components/NavCategoryList.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
-import { useFetchCategoriesReagions } from "../hooks/useFetchCategoriesReagions"
+// import { useFetchCategoriesReagions } from "../hooks/useFetchCategoriesReagions"
 import { CategoryDialog } from "./CategoryDialog"
 import { NavCategorySelect } from "./NavCategorySelect"
 import { DialogContext } from "../contexts/DialogContext"
 
 export const NavCategoryList = () => {
-  const { categories, subCategories } = useFetchCategoriesReagions()
-  const [selectedRegion, setSelectedRegion] = useState("hela_sverige")
+  // const { categories, subCategories } = useFetchCategoriesReagions()
+  // const [selectedRegion, setSelectedRegion] = useState("hela_sverige")
 
   const [isOpen, setIsOpen] = useState(false)
 
