43527f989b746b8349744e7a1ec2af194aa644cd
JanisSeibutis <janis.seibutis@gmail.com>
2025-08-24 12:49:36 +0200

    feat (client): add base route for dev and github pages

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -10,51 +10,56 @@ import { adsLoader } from "./loaders/adsLoader"
 import { adLoader } from "./loaders/adLoader"
 import { CreateAd } from "./pages/CreateAd/CreateAd"
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/auth",
+          children: [
+            {
+              path: "login",
+              element: <Login />,
+            },
+            {
+              path: "register",
+              element: <Register />,
+            },
+          ],
+        },
+        {
+          path: "/ads",
+          children: [
+            {
+              path: "create-ad",
+              element: <CreateAd />,
+            },
+            {
+              path: "",
+              loader: adsLoader,
+              element: <Ads />,
+            },
+            {
+              path: ":id",
+              loader: adLoader,
+              element: <Ad />,
+            },
+          ],
+        },
+        {
+          path: "/user",
+          element: <User />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/auth",
-        children: [
-          {
-            path: "login",
-            element: <Login />,
-          },
-          {
-            path: "register",
-            element: <Register />,
-          },
-        ],
-      },
-      {
-        path: "/ads",
-        children: [
-          {
-            path: "create-ad",
-            element: <CreateAd />,
-          },
-          {
-            path: "",
-            loader: adsLoader,
-            element: <Ads />,
-          },
-          {
-            path: ":id",
-            loader: adLoader,
-            element: <Ad />,
-          },
-        ],
-      },
-      {
-        path: "/user",
-        element: <User />,
-      },
-    ],
-  },
-])
+    basename: import.meta.env.PROD ? "/Swedad" : "/",
+  }
+)
