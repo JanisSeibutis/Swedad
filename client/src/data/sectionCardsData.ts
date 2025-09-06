03ef0203dac55959c327eccc32d5e9c6133cf8e8
JanisSeibutis <janis.seibutis@gmail.com>
2025-09-06 21:22:19 +0200

    feat: add section images

---
+++ client/src/data/sectionCardsData.ts
@@ -0,0 +1,21 @@
+import Balcony from "../assets/images/card-images/balcony.jpg"
+import Box from "../assets/images/card-images/box.jpg"
+import Car from "../assets/images/card-images/car.jpg"
+import Girl from "../assets/images/card-images/girl.jpg"
+import Kopnu from "../assets/images/card-images/kopnu.jpg"
+import Parcel from "../assets/images/card-images/parcel.jpg"
+import Room from "../assets/images/card-images/room.jpg"
+import Used from "../assets/images/card-images/used.jpg"
+import Vardera from "../assets/images/card-images/vardera.jpg"
+
+export const sectionCards = [
+  { imageSrc: Girl },
+  { imageSrc: Car },
+  { imageSrc: Vardera },
+  { imageSrc: Kopnu },
+  { imageSrc: Parcel },
+  { imageSrc: Box },
+  { imageSrc: Used },
+  { imageSrc: Balcony },
+  { imageSrc: Room },
+]
